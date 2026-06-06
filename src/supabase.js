f633ddd529423c0aa84c4eb2849ea6cfeefb6137
soroushhp <soroush.hashemipanah@gmail.com>
2026-06-06 17:20:07 +0200

    Version 2.0 Beta

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://sqgkxntbywjhdpxbosgp.supabase.co";
 const supabaseKey = "sb_publishable_DqJmb6V_5TZ2U1kB8H3ACg_Es12uDEQ";
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
